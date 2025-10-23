9dd1114aaca2a7ab2d13d4f92344e3fb23be1d0a
Azim <azimvafadari@gmail.com>
2025-10-23 19:09:47 +0330

    feat(urlhelper): add `ResolveURL` function

--- internal/basic-crawler-yazd-uni/urlhelper/urlhelper.go
+++ internal/basic-crawler-yazd-uni/urlhelper/urlhelper.go
@@ -1,1 +1,20 @@
 package urlhelper
+
+import (
+	"log"
+	"net/url"
+)
+
+// ResolveURL takes the page's base URL and a link string (href)
+// and returns a complete, absolute URL.
+// It returns 'nil' if the href is invalid.
+func ResolveURL(baseURL *url.URL, href string) *url.URL {
+	resolvedURL, err := baseURL.Parse(href)
+
+	if err != nil {
+		log.Printf("WARNNING: Could not parse %s based on %s: %v", href, baseURL, err)
+		return nil
+	}
+
+	return resolvedURL
+}
